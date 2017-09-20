c44a2304fbbf40b7f58cff0b15395a62b391c1f6
Iong Loi <Iongl@ih.gov.mo>
2017-09-20 14:56:25 +0800

    add code to app.js

--- src/js/app.js
+++ src/js/app.js
@@ -36,9 +36,13 @@ App = {
   },
 
   initContract: function() {
-    /*
-     * Replace me...
-     */
+    $.getJSON('Adoption.json', function(data){
+      var AdoptionArtifact = data;
+      App.contracts.Adoption = TruffleContract(AdoptionArtifact);
+      App.contracts.Adoption.setProvider(App.web3Provider);
+      return ApplicationCache.markAdopted();
+    })
+    
 
     return App.bindEvents();
   },
@@ -51,16 +55,41 @@ App = {
     event.preventDefault();
 
     var petId = parseInt($(event.target).data('id'));
-
-    /*
-     * Replace me...
-     */
+    var adoptionInstance;
+    
+    web3.eth.getAccounts(function(error, accounts) {
+      if (error) {
+        console.log(error);
+      }
+    
+      var account = accounts[0];
+    
+      App.contracts.Adoption.deployed().then(function(instance) {
+        adoptionInstance = instance;
+    
+        return adoptionInstance.adopt(petId, {from: account});
+      }).then(function(result) {
+        return App.markAdopted();
+      }).catch(function(err) {
+        console.log(err.message);
+      });
+    });
   },
 
   markAdopted: function(adopters, account) {
-    /*
-     * Replace me...
-     */
+    var adoptionInstance;
+    App.contracts.Adoption.deployed().then(function(instance){
+      adoptionInstance = instance;
+      return adoptionInstance.getAdopters.call();
+    }).then(function(adopters){
+      for (i = 0; i < adopters.length; i++) {
+        if (adopters[i] !== '0x0000000000000000000000000000000000000000') {
+          $('.panel-pet').eq(i).find('button').text('Pending...').attr('disabled', true);
+        }
+      }
+    }).catch(function(err) {
+      console.log(err.message);
+    });
   }
 
 };
